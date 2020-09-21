284dd51a367a7ff726793eff45e512d11e34adb2
hss-smee <seanmee8186@gmail.com>
2020-09-21 10:00:09 -0700

    Mutations section

--- schema/schema.js
+++ schema/schema.js
@@ -1,19 +1,16 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/Book');
+const Author = require('../models/Author');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
-
-const books = [
-    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
-]
-
-const authors = [
-    {name: 'Patrick Rothfuss', age: 44, id: '1'},
-    {name: 'Brandon Sanderson', age: 42, id: '2'},
-    {name: 'Terry Pratchett', age: 66, id: '3'},
-]
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
+} = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -21,7 +18,13 @@ const BookType = new GraphQLObjectType({
         return {
             id: { type: GraphQLID },
             name: { type: GraphQLString },
-            genre: { type: GraphQLString }
+            genre: { type: GraphQLString },
+            author: {
+                type: AuthorType,
+                resolve(parent, args) {
+                    // return _.find(authors, {id: parent.authorId});
+                }
+            }
         }
     }
 });
@@ -32,7 +35,13 @@ const AuthorType = new GraphQLObjectType({
         return {
             id: { type: GraphQLID },
             name: { type: GraphQLString },
-            age: { type: GraphQLInt }
+            age: { type: GraphQLInt },
+            books: {
+                type: GraphQLList(BookType),
+                resolve(parent, args) {
+                    // return _.filter(books, {authorId: parent.id})
+                }
+            }
         }
     }
 });
@@ -40,23 +49,56 @@ const AuthorType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        books: {
+            type: GraphQLList(BookType),
+            resolve(parent, args) {
+                // return books;
+            }
+        },
+        authors: {
+            type: GraphQLList(AuthorType),
+            resolve(parent, args) {
+                // return authors;
+            }
+        },
         book: {
             type: BookType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-               return _.find(books, {id: args.id});
+               // return _.find(books, {id: args.id});
             }
         },
         author: {
             type: AuthorType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                return _.find(authors, {id: args.id});
+                // return _.find(authors, {id: args.id});
+            }
+        }
+    }
+})
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                const author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
             }
         }
     }
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const {username, password, database} = require('./keys');
+
+mongoose.connect(`mongodb+srv://${username}:${password}@wbcluster.nxeyu.mongodb.net/${database}?retryWrites=true&w=majority`, {useUnifiedTopology: true, useNewUrlParser: true})
+
+mongoose.connection.once('open', () => {
+    console.log("connected to database");
+})
 
 const app = express();
 
